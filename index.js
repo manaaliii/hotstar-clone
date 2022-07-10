0946936645c6dc91aa081c7369cf2adb9f23d9b2
Manali <87221209+manaaliii@users.noreply.github.com>
2022-07-10 15:19:49 +0530

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        let video = item.children[1];
+        video.play();
+    });
+    item.addEventListener("mouseout", () => {
+        let video = item.children[1];
+        video.pause();
+    });
+});
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+    });
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    });
+});
